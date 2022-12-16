5c5b81adacaefbc6caa9ddd5d29b2a333ff8066f
Hữu Thọ CS <huutho.cs@gmail.com>
2022-12-16 11:18:53 +0700

    11h18am16-12-22

---
+++ OnThi/Cau2.py
@@ -0,0 +1,6 @@
+N = int(input("Nhập vào số nguyên dương (2-9): "))
+
+while N < 2 or N > 9:
+    N = int(input("Nhập lại số nguyên dương (2-9): "))
+
+# Bội số chẵn và nhỏ hơn 100 theo thứ tự giảm dần...
